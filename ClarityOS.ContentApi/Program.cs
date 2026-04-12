4dae9e44bc2efac7d2a506442ae865e1820d8355
exikoz <hussein.hasnawy@gmail.com>
2026-04-12 16:57:15 +0200

    feat: scaffold solution with ContentApi and AiProxyApi, add Scalar, fix ports and middleware order

--- ClarityOS.ContentApi/Program.cs
+++ ClarityOS.ContentApi/Program.cs
@@ -1,23 +1,22 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
